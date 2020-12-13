6a67f737b5be1be40db90660069546fe4d2202a9
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2020-12-13 21:23:25 +0500

    off events

--- client/src/components/Layout/LayoutComponent.js
+++ client/src/components/Layout/LayoutComponent.js
@@ -5,7 +5,7 @@ import { Link, Redirect, Route, Switch } from 'react-router-dom';
 import {
   DollarCircleOutlined,
   LogoutOutlined,
-  NotificationOutlined,
+  // NotificationOutlined,
   OrderedListOutlined,
   ShopOutlined,
   UserOutlined,
@@ -74,16 +74,16 @@ const LayoutComponent = () => {
               >
                 <Link to="/students">Рейтинг студентов</Link>
               </Menu.Item>
-              <Menu.Item
-                key="4"
-                icon={<NotificationOutlined className={style.iconColor} />}
-              >
-                <a
-                  href={`${process.env.REACT_APP_URL_DEFAULT}:4000/events?role=${user.status}`}
-                >
-                  Эвенты не работают
-                </a>
-              </Menu.Item>
+              {/*<Menu.Item*/}
+              {/*  key="4"*/}
+              {/*  icon={<NotificationOutlined className={style.iconColor} />}*/}
+              {/*>*/}
+              {/*  <a*/}
+              {/*    href={`${process.env.REACT_APP_URL_DEFAULT}:4000/events?role=${user.status}`}*/}
+              {/*  >*/}
+              {/*    Эвенты не работают*/}
+              {/*  </a>*/}
+              {/*</Menu.Item>*/}
               {user.status === 'Гость' ? null : (
                 <Menu.Item
                   key="5"
